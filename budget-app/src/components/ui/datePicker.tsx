38d99153e023f692c3bb2ba69a5eed0e8632624a
1saifu <saifukhan@hotmail.se>
2024-11-19 22:12:23 +0100

    date file

---
+++ budget-app/src/components/ui/datePicker.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import React from "react";
+import { DateRange } from "react-date-range";
+import { enUS } from "react-date-range/dist/locale";
+import "react-date-range/dist/styles.css"; 
+import "react-date-range/dist/theme/default.css"; 
+import './style/date.css'
+
+type DateRangeType = {
+    startDate: Date;
+    endDate: Date;
+    key: string;
+};
+
+interface DatePickerProps {
+    dateRange: DateRangeType;
+    onChange: (range: DateRangeType) => void;
+}
+
+const DatePicker: React.FC<DatePickerProps> = ({ dateRange, onChange }) => {
+    return (
+        <div className="date-range-picker"> 
+            <DateRange
+                ranges={[dateRange]}
+                onChange={(ranges) => onChange(ranges.selection as DateRangeType)}
+                direction="vertical"
+                showDateDisplay={false}
+                rangeColors={['#800080', '#800080']}                 
+                locale={enUS}
+            />
+        </div>
+    );
+};
+
+export default DatePicker;
